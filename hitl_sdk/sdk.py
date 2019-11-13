3c113fd2b09126c5bf17eec68a9eb9aa040d55d8
NilhEx <nilhex@protonmail.com>
2019-11-13 18:10:39 +0300

    support field_name and document_name params; switch to dataclass

--- hitl_sdk/sdk.py
+++ hitl_sdk/sdk.py
@@ -1,26 +1,72 @@
+from typing import *
+
+from dataclasses import dataclass, field
+import dataclasses_json
+
 import asyncio
 import base64
-from dataclasses import dataclass, field
+import datetime
 
 import aiohttp
+
+
+@dataclasses_json.dataclass_json
+@dataclass
+class Task:
+    id: Optional[str] = None
+    result: Optional[str] = None
+    field_name: Optional[str] = None
+    code: Optional[str] = None
+    created_at: Optional[datetime.datetime] = field(
+        default=None,
+        metadata=dataclasses_json.config(
+            decoder=lambda _: datetime.datetime.fromisoformat(_) if _ else None
+        ),
+    )
+    completed_at: Optional[datetime.datetime] = field(
+        default=None,
+        metadata=dataclasses_json.config(
+            decoder=lambda _: datetime.datetime.fromisoformat(_) if _ else None
+        ),
+    )
+
+    # Using when creating
+    img: Optional[Union[str, bytes]] = None
+    predict: Optional[str] = None
+    type: str = 'standard'
+    pipeline: List[str] = field(default_factory=lambda: ['validation', 'ocr'])
 
 
 @dataclass
 class SDK:
     host: str
-    token: str = None
-    system_info_token: str = None
-    tasks: dict = field(default_factory=lambda: {})
+    token: Optional[str] = None
+    system_info_token: Optional[str] = None
+    tasks: Dict[str, Task] = field(default_factory=lambda: {})
 
-    async def _request(self, method: str, params: dict = {}, data: dict = {}) -> [dict]:
+    @staticmethod
+    def _get_task_key(task: Task) -> str:
+        return (
+            f'{task.id}:{task.field_name}'
+            if task.field_name
+            else
+            task.id
+        )
+
+    async def _request(self,
+                       method: str,
+                       params: Optional[dict] = None,
+                       data: Optional[Union[dict, list]] = None) -> [dict]:
         headers = {
             'Content-Type': 'application/json',
         }
+        if params is None:
+            params = {}
+
         if self.token:
             headers['Authorization'] = f'Token {self.token}'
         if self.system_info_token:
             params['system_info'] = self.system_info_token
-
 
         async with aiohttp.ClientSession() as session:
             async with session.request(
@@ -38,27 +84,31 @@ class SDK:
                 return await resp.json()
 
     async def create_tasks(
-        self,
-        tasks: [dict],
-        document_id: str = None,
-        task_type: str = 'standard',
-        mock: bool = False
-    ) -> [dict]:
+            self,
+            tasks: List[Task],
+            document_name: Optional[str] = None,
+            document_id: Optional[str] = None,
+            task_type: Optional[str] = 'standard',
+            mock: bool = False
+    ) -> List[Task]:
         body = [
             {
                 'img': base64.b64encode(
-                    task['img'],
-                ).decode() if isinstance(task['img'], bytes) else task['img'],
-                'predict': task.get('predict'),
-                'code': task.get('code'),
+                    task.img,
+                ).decode() if isinstance(task.img, bytes) else task.img,
+                'predict': task.predict,
                 'type': task_type,
-                'document_id': task.get('document_id') if not document_id else document_id,
-                'pipeline': task.get('pipeline') if not mock else ['mock'],
+                'field_name': task.field_name,
+                'document_name': document_name,
+                'code': task.code,
+                'document_id': document_id,
+                'pipeline': task.pipeline if not mock else ['mock'],
             }
             for task in tasks
-            if task.get('img')
+            if task.img
         ]
-        if not body: return []
+        if not body:
+            return []
 
         tasks = await self._request(
             method='POST',
@@ -66,16 +116,21 @@ class SDK:
         )
 
         for task in tasks:
-            self.tasks[task['id']] = task
+            task = Task.from_dict(task)
+            self.tasks[self._get_task_key(task)] = task
 
         return list(self.tasks.values())
 
     async def sync_tasks(self):
-        tasks_ids = list(filter(
-            lambda _: not self.tasks.get(_) or not self.tasks[_]['completed_at'],
-            self.tasks.keys(),
-        ))
-        if not tasks_ids: return
+        tasks_ids = [
+            _id.split(':')[0]
+            for _id in filter(
+                lambda _: not self.tasks.get(_) or not self.tasks[_].completed_at,
+                self.tasks.keys(),
+            )
+        ]
+        if not tasks_ids:
+            return
 
         try:
             tasks = await self._request(
@@ -86,7 +141,8 @@ class SDK:
             )
 
             for task in tasks:
-                self.tasks[task['id']] = task
+                task = Task.from_dict(task)
+                self.tasks[self._get_task_key(task)] = task
         except Exception as e:
             print(e)
 
@@ -94,19 +150,22 @@ class SDK:
         return sum(
             1
             for v in self.tasks.values()
-            if not v['completed_at']
+            if not v.completed_at
         )
 
-    async def wait_until_complete(self, timeout: float = 5.) -> [dict]:
+    async def wait_until_complete(self, timeout: float = 5.) -> List[Task]:
         while self.in_work_count() != 0:
             print(f'In work {self.in_work_count()} tasks. Sync...')
             await self.sync_tasks()
             await asyncio.sleep(timeout)
         return list(self.tasks.values())
 
-    async def create_and_wait(self, tasks: list, timeout: float = 5., **kwargs) -> [dict]:
+    async def create_and_wait(self,
+                              tasks: List[Task], document_name: Optional[str] = None,
+                              timeout: float = 5., **kwargs) -> List[Task]:
         await self.create_tasks(
             tasks=tasks,
+            document_name=document_name,
             **kwargs,
         )
         return await self.wait_until_complete(timeout=timeout)
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 
 install_requires = [
-    'aiohttp',
+    'aiohttp==3.5.4',
+    'dataclasses-json==0.3.5',
 ]
 
 CONFIG = {
     'name': 'hitl-sdk',
     'url': '',
-    'version': '0.1.0',
+    'version': '0.2.0',
     'author': 'NilhEx',
     'install_requires': install_requires,
     'packages': find_packages(),
