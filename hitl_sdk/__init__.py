ba5a0de0e8aaacb50b36b2bc506822590a7c1094
David Kuryakin <dkuryakin@gmail.com>
2022-04-02 03:29:20 +0300

    initial

--- hitl_sdk/__init__.py
+++ hitl_sdk/__init__.py
@@ -1,1 +1,11 @@
-from .sdk import SDK, Task
+from .common import Task
+from .env import HITL_BACKEND
+from .handl.sdk import SDK as HandlSDK
+from .toloka.sdk import SDK as TolokaSDK
+
+if HITL_BACKEND == 'toloka':
+    SDK = TolokaSDK
+elif HITL_BACKEND == 'handl':
+    SDK = HandlSDK
+else:
+    raise AssertionError('HITL_BACKEND env value is not in list: [toloka, handl]')
